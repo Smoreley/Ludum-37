d16eb2e9aa550e7981835b88a1ac46435b2cc37d
skyler <Smoreley@gmail.com>
2016-12-11 12:02:36 -0500

    update

--- src/states/game.js
+++ src/states/game.js
@@ -1,54 +1,93 @@
 class Game {
     constructor() {
         this.name = "game_state";
-        this.speed = 4;
+        this.speed = 40;
         this.dir = 1;
-        this.rotationSpeed = 75;
+        this.rotationSpeed = 90;
     }
     
     preload () {
         game.load.image('houseimage', 'bin/imgs/character64.png');
-        game.load.image('backdrop', 'bin/imgs/bg_map.png');
+//        game.load.image('backdrop', 'bin/imgs/bg_map2.png');
+        
+        game.load.atlasJSONHash('bot', 'bin/imgs/mainChar.png', 'bin/imgs/mainChar.json');
+        
+//        game.load.json('myKey', 'bin/imgs/ludMap37.json');
+        
+        game.load.tilemap('mario', 'bin/imgs/ludMap37.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'bin/imgs/mapTile.png');
+        
     }
     
     create () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.graphics = game.add.graphics(0,0);
         
+        game.stage.backgroundColor = '#787878';
         
-        game.world.setBounds(0, 0, 2048, 2048);
-        game.add.sprite(0, 0, 'backdrop');
+            // Background Map
+        this.map = game.add.tilemap('mario');
+        this.map.addTilesetImage('Main Map', 'tiles');
+        this.ground_layer = this.map.createLayer('Ground');
+        this.object_layer = this.map.createLayer('Objects');
         
-        this.house = game.add.sprite(0, 0, 'houseimage');
+        this.ground_layer.resizeWorld();
+        this.ground_layer.scale.setTo(2,2);
+        this.object_layer.resizeWorld();
+        this.object_layer.scale.setTo(2,2);
+        
+        
+        game.world.setBounds(0, 0, 4096, 2048);
+//        game.add.sprite(0, 0, 'backdrop');
+        
+//        this.house = game.add.sprite(0, 0, 'houseimage');
+//        this.house.anchor.setTo(0.5, 0.5);
+//        
+//        game.physics.enable(this.house, Phaser.Physics.ARCADE);
+//        this.house.f = new Phaser.Point(1,0);
+//        this.newDirection = new Phaser.Point(0,0);
+        
+        
+        
+        this.house = game.add.sprite(0, 0, 'bot');
         this.house.anchor.setTo(0.5, 0.5);
-        
+        this.house.scale.setTo(2, 2);
+        this.house.animations.add('run');
+        this.house.animations.play('run', 16, true);
         game.physics.enable(this.house, Phaser.Physics.ARCADE);
         this.house.f = new Phaser.Point(1,0);
-        
         this.newDirection = new Phaser.Point(0,0);
-        
         game.camera.follow(this.house);
+        
+        
+//        this.bot = game.add.sprite(512, 512, 'bot');
+//        this.bot.scale.setTo(2, 2);
+//        this.bot.animations.add('run');
+//        this.bot.animations.play('run', 16, true);
     }
     
     update () {
         var deltaTime = (game.time.elapsed/1000.0);
 
+        console.log(this.house.body.speed);
+        
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
         {
-            this.house.angle -= this.rotationSpeed * deltaTime;
+            this.house.angle -= this.rotationSpeed * deltaTime + (0.01*(this.house.body.speed));
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
         {
-            this.house.angle += this.rotationSpeed * deltaTime;
+            this.house.angle += this.rotationSpeed * deltaTime + (0.01*(this.house.body.speed));
         }
         
         // Push away
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
         {
-            move(this.house, this.speed);
-        } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+            move(this.house, 1);
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
         {
-            move(this.house, -this.speed);            
+            move(this.house, -1);            
         } else {
             this.house.f = new Phaser.Point(0,0);
         }
@@ -62,7 +101,7 @@ class Game {
         
         this.house.body.velocity.x += this.house.f.x * this.speed;
         this.house.body.velocity.y += this.house.f.y * this.speed;
-        this.house.body.velocity = this.house.body.velocity.multiply(0.95,0.95);
+        this.house.body.velocity = this.house.body.velocity.multiply(0.9,0.9);
 //        
 //        var vTwo = new Phaser.Point(this.house.x, this.house.y);
 //        var vOne = new Phaser.Point(10,20);
@@ -81,6 +120,6 @@ class Game {
 }
 
 function move(object, speed) {
-    object.f = new Phaser.Point(Math.cos(object.rotation), Math.sin(object.rotation));
-    object.f = object.f.multiply(speed,speed);    
+    object.f = new Phaser.Point(Math.cos(object.rotation-Math.PI/2), Math.sin(object.rotation-Math.PI/2));
+    object.f = object.f.multiply(speed,speed); 
 }
